3f1dd636eadb9f76bfafd0f0c5e0bd97ed1881a6
Valérien <pro@brayevalerien.com>
2026-03-23 15:47:29 +0100

    initial keymap

---
+++ config.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define OLED_TIMEOUT 0
+#define SPLIT_WPM_ENABLE
+#define SPLIT_ENCODER_ENABLE
+
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#define RGB_MATRIX_DEFAULT_SPD 40
